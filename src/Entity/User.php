6b08ec2101530eced6f80926bafee0a5ec43d03c
RangoDisco <diasmaxime1@gmail.com>
2026-06-25 21:01:24 +0200

    feat: handled user security config

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,12 +6,17 @@ use App\Enum\UserRole;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User extends GenericEntity
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User extends GenericEntity implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -25,8 +30,8 @@ class User extends GenericEntity
     #[ORM\Column(length: 255)]
     private ?string $password = null;
 
-    #[ORM\Column(enumType: UserRole::class)]
-    private UserRole $role = UserRole::User;
+    #[ORM\Column(type: Types::SIMPLE_ARRAY)]
+    private array $roles = [];
 
     #[ORM\ManyToOne]
     private ?Media $picture = null;
@@ -42,9 +47,19 @@ class User extends GenericEntity
     #[ORM\OneToMany(targetEntity: Prescription::class, mappedBy: 'patient')]
     private Collection $prescriptions;
 
+    /**
+     * @var Collection<int, Doctor>
+     */
+    #[ORM\OneToMany(targetEntity: Doctor::class, mappedBy: 'owner', orphanRemoval: true)]
+    private Collection $doctors;
+
+    #[ORM\Column]
+    private bool $isVerified = false;
+
     public function __construct()
     {
         $this->prescriptions = new ArrayCollection();
+        $this->doctors = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -64,6 +79,9 @@ class User extends GenericEntity
         return $this;
     }
 
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -76,16 +94,33 @@ class User extends GenericEntity
         return $this;
     }
 
-    public function getRole(): ?UserRole
+    /**
+     * @return UserRole[]
+     */
+    public function getRoles(): array
     {
-        return $this->role;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = UserRole::User->value;
+
+        return array_unique($roles);
     }
 
-    public function setRole(UserRole $role): static
+    public function setRoles(array $roles): static
     {
-        $this->role = $role;
+        $this->roles = $roles;
 
         return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
     }
 
     public function getPicture(): ?Media
@@ -138,6 +173,48 @@ class User extends GenericEntity
                 $prescription->setPatient(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Doctor>
+     */
+    public function getDoctors(): Collection
+    {
+        return $this->doctors;
+    }
+
+    public function addDoctor(Doctor $doctor): static
+    {
+        if (!$this->doctors->contains($doctor)) {
+            $this->doctors->add($doctor);
+            $doctor->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDoctor(Doctor $doctor): static
+    {
+        if ($this->doctors->removeElement($doctor)) {
+            // set the owning side to null (unless already changed)
+            if ($doctor->getOwner() === $this) {
+                $doctor->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
