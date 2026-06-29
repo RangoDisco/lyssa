b9e4e66049c6a8bc52b37261b28bab74b5f58339
RangoDisco <diasmaxime1@gmail.com>
2026-06-29 08:23:30 +0200

    fix(doctor): updated voter const key and handled missing delete case

--- src/Security/Voter/DoctorVoter.php
+++ src/Security/Voter/DoctorVoter.php
@@ -1,23 +1,30 @@
 <?php
 
-namespace App\Security;
+namespace App\Security\Voter;
 
 use App\Entity\Doctor;
 use App\Entity\User;
 use App\Enum\CaretakerAccessLevel;
+use App\Enum\UserRole;
 use App\Repository\CaretakingAccessRepository;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Exception\LogicException;
 
 class DoctorVoter extends Voter
 {
-    public const DELETE = 'delete';
-    public const EDIT = 'edit';
-    public const SHOW = 'show';
+    public const string DELETE = 'DOCTOR_DELETE';
+    public const string EDIT = 'DOCTOR_EDIT';
+    public const string SHOW = 'DOCTOR_SHOW';
 
-    public function __construct(private readonly CaretakingAccessRepository $ca){}
+    public function __construct(
+        private readonly CaretakingAccessRepository     $ca,
+        private readonly AccessDecisionManagerInterface $adm,
+    )
+    {
+    }
 
     public function supports(string $attribute, mixed $subject): bool
     {
@@ -37,10 +44,15 @@ class DoctorVoter extends Voter
             return false;
         }
 
+        if ($this->adm->decide($token, [UserRole::Admin->value])) {
+            return true;
+        }
+
         return match ($attribute) {
             self::SHOW => $this->canView($subject, $user, $vote),
             // EDIT also handle delete (for now)
             self::EDIT => $this->canEdit($subject, $user, $vote),
+            self::DELETE => $this->canDelete($subject, $user, $vote),
             default => throw new LogicException("Invalid attribute provided")
         };
     }
@@ -68,6 +80,22 @@ class DoctorVoter extends Voter
         }
 
         $vote->addReason("Only owners, or caretakers with edit rights can edit a doctor");
+
+        return false;
+    }
+
+    private function canDelete(Doctor $doctor, User $user, Vote $vote): bool
+    {
+        if ($doctor->getOwner() === $user) {
+            return true;
+        }
+
+        // Current user should have an existing CaretakingAccess with Edit rights and the given doctor's owner as the patient.
+        if ($this->ca->isCaretakerOf($user, $doctor->getOwner(), CaretakerAccessLevel::Edit)) {
+            return true;
+        }
+
+        $vote->addReason("Only owners, or caretakers with edit rights can delete a doctor");
 
         return false;
     }
