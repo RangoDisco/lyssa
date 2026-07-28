13e8842feb28fdb1ebc5e0885df9a53c2dbc23b1
RangoDisco <diasmaxime1@gmail.com>
2026-07-28 21:01:55 +0200

    feat(substance): added main and category entities

---
+++ src/Entity/Substance.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubstanceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubstanceRepository::class)]
+class Substance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $code = null;
+
+    /**
+     * @var Collection<int, SubstanceCategory>
+     */
+    #[ORM\ManyToMany(targetEntity: SubstanceCategory::class, inversedBy: 'substances')]
+    private Collection $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, SubstanceCategory>
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(SubstanceCategory $category): static
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(SubstanceCategory $category): static
+    {
+        $this->categories->removeElement($category);
+
+        return $this;
+    }
+}
