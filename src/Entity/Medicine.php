9961cb31bd4957cafd06208daecd30c7b07f79e6
RangoDisco <diasmaxime1@gmail.com>
2026-08-05 13:55:37 +0200

    fix(medicine): switched name from varchar to text

--- src/Entity/Medicine.php
+++ src/Entity/Medicine.php
@@ -7,6 +7,7 @@ use App\Enum\Source;
 use App\Repository\MedicineRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -19,7 +20,7 @@ class Medicine extends GenericEntity
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     #[Assert\NotBlank]
     private ?string $name = null;
 
