c645b8c30ed260fa3c7d735625fa28daab21b3c8
RangoDisco <diasmaxime1@gmail.com>
2026-08-16 13:00:22 +0200

    feat(invitation): also logged catch errors

--- src/Controller/InvitationController.php
+++ src/Controller/InvitationController.php
@@ -8,23 +8,26 @@ use App\Form\InvitationJoinType;
 use App\Form\InvitationType;
 use App\Service\InvitationService;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
-use Symfony\Component\Mailer\Exception\HttpTransportException;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Uid\Uuid;
+use Throwable;
 
 #[Route('/invitation')]
 final class InvitationController extends AbstractController
 {
 
     public function __construct(
-        private readonly InvitationService $invitationService
+        private readonly InvitationService $invitationService,
+        private readonly LoggerInterface   $logger
     )
     {
     }
@@ -45,6 +48,7 @@ final class InvitationController extends AbstractController
     #[IsGranted('IS_AUTHENTICATED')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        // TODO: handle already existing user
         $invitation = new Invitation();
         $form = $this->createForm(InvitationType::class, $invitation);
         $form->handleRequest($request);
@@ -54,15 +58,17 @@ final class InvitationController extends AbstractController
 
             try {
                 $this->invitationService->send($invitation->getPatient(), $form->get("email")->getData(), $invitation->getToken());
-            } catch (HttpTransportException) {
-                $form->addError(new FormError("Unable to send mail. Please try again later"));
-            }
 
-            if ($form->isValid()) {
                 $entityManager->persist($invitation);
                 $entityManager->flush();
 
                 return $this->redirectToRoute('app_invitation_index', [], Response::HTTP_SEE_OTHER);
+            } catch (TransportExceptionInterface $e) {
+                $this->logger->error('Failed to send invitation link', [
+                    'exception' => $e,
+                    'patient' => $invitation->getPatient()->getId()
+                ]);
+                $form->addError(new FormError("Unable to send mail. Please try again later"));
             }
         }
 
@@ -76,23 +82,23 @@ final class InvitationController extends AbstractController
     public function join(
         Invitation $invitation,
         Security   $security,
-        Request    $request
+        Request    $request,
     ): Response
     {
         $form = $this->createForm(InvitationJoinType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $invitation->setToken(Uuid::v4());
-
             try {
                 $user = $this->invitationService->join($invitation, $form->get("username")->getData(), $form->get('plainPassword')->getData());
-            } catch (HttpTransportException) {
-                $form->addError(new FormError("Unable to register. Please try again later"));
-            }
 
-            if ($form->isValid()) {
                 return $security->login($user, 'form_login', 'main');
+            } catch (Throwable $e) {
+                $this->logger->error("Unable to join", [
+                    'exception' => $e,
+                    'invitation' => $invitation->getId()
+                ]);
+                $form->addError(new FormError("Unable to register. Please try again later"));
             }
         }
 
@@ -101,7 +107,6 @@ final class InvitationController extends AbstractController
             'form' => $form,
         ]);
     }
-
 
     #[Route('/{id}', name: 'app_invitation_show', methods: ['GET'])]
     #[IsGranted('INVITATION_VIEW', subject: 'invitation')]
